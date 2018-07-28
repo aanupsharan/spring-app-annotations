ee05c30f65d1af534a5132f84e503c0abb0d782c
aanupsharan <anup.ambula@gmail.com>
2018-07-28 11:43:10 -0500

    added the fortune implementation

---
+++ src/com/anup/springdemo/RandomFortuneService.java
@@ -0,0 +1,11 @@
+package com.anup.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RandomFortuneService implements FortuneService {
+	@Override
+	public String getFortune() {
+		return "This is the random Fortune Service";
+	}
+}
---
+++ src/com/anup/springdemo/RestfulFortuneService.java
@@ -0,0 +1,12 @@
+package com.anup.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RestfulFortuneService implements FortuneService {
+	@Override
+	public String getFortune() {
+		// TODO Auto-generated method stub
+		return "This is Restful Fortune Service";
+	}
+}
