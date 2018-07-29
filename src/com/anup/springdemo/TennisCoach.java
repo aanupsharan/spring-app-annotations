1eca54c5ed457492fdef054ab93961b7fcc53539
aanupsharan <anup.ambula@gmail.com>
2018-07-29 12:26:58 -0500

    added the @PostConstructor and @PreDestroy methods in bean life cycle

--- src/com/anup/springdemo/TennisCoach.java
+++ src/com/anup/springdemo/TennisCoach.java
@@ -1,16 +1,27 @@
 package com.anup.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
 public class TennisCoach implements Coach {
 	@Autowired
 	@Qualifier("randomFortuneService")
 	private FortuneService fortuneService;
+	
+	@PostConstruct
+	public void doMyStartUpStuff() {
+		System.out.println("TennisCoach ::: in the doMyStartUpStuff() method");
+	}
+
+	@PreDestroy
+	public void doMyCleanUpStuff() {
+		System.out.println("TennisCoach ::: in the doMyCleanUpStuff() method");
+	}
 	
 	@Override
 	public String getDailyWorkout() {
